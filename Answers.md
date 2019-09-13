2a89396f26c589d20524ec2648f99b61faec7d08
Justin Kane <JustinKane197@gmail.com>
2019-09-13 13:55:27 -0400

    answered questions and reached MVP

--- Answers.md
+++ Answers.md
@@ -1,11 +1,12 @@
 # Answers
 
 1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
-
+react is a javascript library that allows us to simplify a complex UI. It makes large scale apps that handle a lot of data easier to build and maintain
 1. What does it mean to think in react?
-
+breaking down an application into smaller, reusable parts to be used as components
 1. Describe state.
-
+an object where component values are stored. determines how the component will render.
 1. Describe props.
-
+arguments/properties that pass data down from component to component
 1. What are side effects, and how do you sync effects in a React component to state or prop changes?
+when a function reaches outside of itself for data. an API is an example of this
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import CharacterCards from "./components/characterCard";
+import styled from "styled-components";
+
+const CardWrap = styled.div`
+  border: 2px solid black;
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [starWarsPeople, setStarWarsPeople] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(res => {
+        setStarWarsPeople(res.data.results);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
   return (
+
     <div className="App">
+
       <h1 className="Header">React Wars</h1>
+
+      <CardWrap>
+        {starWarsPeople.map((people, index) => {
+          return <CharacterCards key={index} people={people} />;
+        })}
+      </CardWrap>
+      
     </div>
   );
 }
